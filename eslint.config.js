981cd950fcc976a3bf0411df43e91525c60a3a2e
이민용 <me@newfolder.games>
2025-06-03 00:43:49 +0900

    린트 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,46 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+	{
+		ignores: ["dist"],
+	},
+	{
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			stylistic.configs.customize({
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+				semi: true,
+				jsx: true,
+			}),
+		],
+		files: ["**/*.{ts,tsx}"],
+		languageOptions: {
+			ecmaVersion: 2023,
+			globals: globals.browser,
+		},
+		plugins: {
+			"react-hooks": reactHooks,
+			"react-refresh": reactRefresh,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			"react-refresh/only-export-components": [
+				"warn",
+				{ allowConstantExport: true },
+			],
+			"@stylistic/no-trailing-spaces": "off",
+			"@stylistic/jsx-one-expression-per-line": "off",
+			"@stylistic/padded-blocks": "off",
+			"@stylistic/spaced-comment": "off",
+			"@typescript-eslint/no-explicit-any": "off",
+		},
+	},
+);
